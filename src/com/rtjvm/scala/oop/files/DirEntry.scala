484cc076df6e9a369d3097600b87665e250fcff4
Hazel Martinez <hazel@MacBook-Pro-de-Hazel.local>
2020-12-13 18:30:52 -0600

    cd command

--- src/com/rtjvm/scala/oop/files/DirEntry.scala
+++ src/com/rtjvm/scala/oop/files/DirEntry.scala
@@ -2,10 +2,19 @@ package com.rtjvm.scala.oop.files
 
 abstract class DirEntry(val parentPath: String, val name: String) {
 
-  def path: String = parentPath + Directory.SEPARATOR + name
+  def path: String = {
+    val separatorIfNecessay =
+      if (Directory.ROOT_PATH.equals(parentPath)) ""
+      else Directory.SEPARATOR
+
+    parentPath + separatorIfNecessay + name
+  }
 
   def asDirectory: Directory
   def asFile: File
+
+  def isDirectory: Boolean
+  def isFile: Boolean
 
   def getType: String
 }
